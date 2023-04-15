2d36c6078042f19b62f85e2f01011e788713b47e
lamiyahell <axira1108@gmail.com>
2023-04-15 09:54:43 +0300

    1 commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        {
+            System.out.println("Домашние задания. Массивы");
+            System.out.println("Задача 1");
+
+
+        }
+    }
+}
