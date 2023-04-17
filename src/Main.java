68f362518a3605160d93b100f32d5cee996cb827
lamiyahell <axira1108@gmail.com>
2023-04-17 21:00:16 +0300

    Домашка по теме Массивы. Урок 2

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,22 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        {
+        task1();
+        task2();
+    }
+        public static void task1() {
             System.out.println("Домашние задания. Массивы");
             System.out.println("Задача 1");
-            int [] box = new int [3];
+            int[] box = new int[3];
             box[0] = 1;
             box[1] = 2;
             box[2] = 3;
             System.out.println(box[1]);
 
-            double [] values = {1.57, 7.654, 9.986};
+            double[] values = {1.57, 7.654, 9.986};
             System.out.println(values[1]);
 
-            int [] balls = {9, 4, 7, 1};
+            int[] balls = {9, 4, 7, 1};
             System.out.println(balls[1]);
 
 
@@ -49,6 +52,7 @@ public class Main {
                     System.out.print(", ");
                 }
             }
+
             System.out.println();
             for (int i = values.length - 1; i >= 0; i--) {
                 System.out.print(values[i]);
@@ -65,6 +69,7 @@ public class Main {
             }
             System.out.println();
 
+
             System.out.println("Задача 4");
             for (int i = 0; i < box.length; i++) {
                 if (box[i] % 2 != 0) {
@@ -73,5 +78,54 @@ public class Main {
             }
             System.out.println(Arrays.toString(box));
         }
+
+    public static int[] generateRandomArray () {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task2() {
+        System.out.println("Домашние задания. Массивы. Урок 2");
+        System.out.println("Задача 1");
+        int[] arr = generateRandomArray();
+        int summa = 0;
+        for (int i = 0; i < arr.length; i++) {
+            summa = summa + arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + summa + " рублей");
+
+
+        System.out.println("Задача 2");
+        int minSumma = 200_001;
+        int maxSumma = -1;
+        for (int salary : arr ) {
+            if (salary < minSumma) {
+                minSumma = salary;
+            }
+            if (salary > maxSumma) {
+                maxSumma = salary;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + minSumma + " рублей. " +
+                "Максимальная сумма трат за день составила " + maxSumma + " рублей");
+
+
+        System.out.println("Задача 3");
+        double averageSumma = 0;
+        averageSumma = (double) summa / arr.length;
+        System.out.println("Средняя сумма трат за месяц составила " + averageSumma + " рублей");
+
+
+        System.out.println("Задача 4");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
+
+
